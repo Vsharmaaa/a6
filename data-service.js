9a9c51f3261d04bb0e1c5e6ce4f6d3c45f4a45cb
Vishesh Sharma <vsharma156@myseneca.ca>
2022-10-30 17:32:26 -0400

    i guess

--- data-service.js
+++ data-service.js
@@ -59,4 +59,95 @@ exports.getDepartments = () => {
     })
 };
 
+exports.addEmployee=(employeeData)=>{
+    if(employeeData.isManager==undefined)
+    {
+        employeeData.isManager=false;
+    }
+    else
+    {
+employeeData.isManager=true;
+    }
+    employeeData.employeeNum=employees.length +1;
+    employees.push(employeeData);
+    return new Promise ((resolve,reject) => {
+        
+       if (employeeData.length == 0) 
+            reject('no results returned');
+       
+            resolve();
+    })
 
+};
+exports.getEmployeesByStatus=(status)=>
+{
+
+    return new Promise ((resolve,reject) => {
+        var stats=[]
+        for(var i=0;i<employees.length;i++)
+       {
+        if(employees[i].status==status)
+        {
+           
+            stats.push(employees[i])
+        }}
+        if (status.length == 0) 
+            reject('no results returned');
+       
+            resolve(stats);
+    })
+}
+exports.getEmployeesByDepartment=(department)=>
+{
+    return new Promise ((resolve,reject) => {
+        var Dept_status=[];
+        for(var i=0;i<employees.length;i++)
+       {
+        if(employees[i].department==department)
+        {
+            
+            Dept_status.push(employees[i]);
+        }}
+        if (Dept_status.length == 0) 
+            reject('no results returned');
+       
+            resolve(Dept_status);
+    })
+}
+exports.getEmployeesByManager=(manager)=>
+{
+
+    return new Promise ((resolve,reject) => {
+        var mang_status=[];
+        for(var i=0;i<employees.length;i++)
+       {
+        if(employees[i].employeeManagerNum==manager)
+        {
+            
+            mang_status.push(employees[i]);
+        }}
+        if (mang_status.length == 0) 
+            reject('no results returned');
+       
+            resolve(mang_status);
+    })
+}
+exports.getEmployeeByNum=(num)=>{
+    return new Promise ((resolve,reject) => {
+    var nums;
+    for(var i=0;i<employees.length;i++)
+    {
+     if(employees[i].employeeNum==num)
+     {
+         
+         nums=employees[i];
+     }}
+
+     if (nums.length == 0) 
+     reject('no results returned');
+
+     resolve(nums);
+
+
+})
+}
--- server.js
+++ server.js
@@ -1,15 +1,43 @@
-
+/*********************************************************************************
+* BTI325 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Vishesh Sharma Student ID: 117431213 Date: 14/09/22
+*
+* Online (Cyclic) URL:
+* https://ruby-friendly-marlin.cyclic.app
+*
+********************************************************************************/ 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 var path = require('path');
 var linkToDataService__ = require(__dirname + "/data-service.js");
 app.use(express.static('public'));
+const fs = require('fs');  
+const multer = require("multer");
 
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename :function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+        } 
+    }
+  );
+  
+  // tell multer to use the diskStorage function for naming files instead of the default.
+  const upload = multer({ storage: storage });
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 onHttpStart = () => {
     console.log('Express http server listening on port ' + HTTP_PORT);
+    
 }
+
 //home
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/views/home.html"));
@@ -31,27 +59,96 @@ app.get("/managers", (req, res) => {
 });
 //employees
 app.get("/employees", (req, res) => {
+if(req.query.status)
+{console.log(req.query.status);
+   
+    linkToDataService__.getEmployeesByStatus(req.query.status).then((stats)=>{
+        res.json({stats});
+     
+    }).catch((err)=>{
+        res.json({err})
+        console.log("jo");
+    })
+}
+else if(req.query.department)
+{
+    linkToDataService__.getEmployeesByDepartment(req.query.department).then((depts)=>{
+        res.json({depts});
+    
+    }).catch((err)=>{
+        res.json({err})
+    })
+
+
+}
+else if(req.query.manager)
+{
+    linkToDataService__.getEmployeesByManager(req.query.manager).then((mang)=>{
+        res.json({mang});
+    
+    }).catch((err)=>{
+        res.json({err})
+    })
+
+}
+else{
+
     linkToDataService__.getAllEmployees().then((data) => {
         res.json({data});
     }).catch((error) => {
         res.json({error});
-    })
+    })}
 });
 //departments
 app.get("/departments", (req, res) => {
     linkToDataService__.getDepartments().then((data) => {
-        res.json({data});
+        res.send(JSON.stringify(data));
     }).catch((error) => {
         res.json({ error});
     })
 });
 
+app.get("/employee/:value",(req,res)=>{
+    linkToDataService__.getEmployeeByNum(req.params.value).then((data) => {
+        res.send(JSON.stringify(data));;
+    }).catch((error) => {
+        res.json({error});
+    })
+
+});
+
+app.get("/employees/add",(req,res)=>
+{
+    res.sendFile(path.join(__dirname + "/views/addEmployee.html"));
+})
+
+app.post('/employees/add', (req,res) => {
+    linkToDataService__.addEmployee(req.body).then(() => {
+        res.redirect("/employees");
+    }).catch((error)=>{
+        res.json({error});
+    })
+});
+app.get("/images/add",(req,res)=>
+{
+    res.sendFile(path.join(__dirname + "/views/addImage.html"));
+})
+app.post("/images/add", upload.single("imageFile"), (req, res) => {
+    res.redirect("/images")
+  });
+  app.get("/images", (req,res) => {
+    fs.readdir("./public/images/uploaded", function(err,items) {
+        res.json(items);
+    })
+});
+
+
 linkToDataService__.initialize().then(() => {
     app.listen(HTTP_PORT, onHttpStart());
+    
 }).catch (() => {
     console.log('Assignment Not Complete');
 });
-//app.listen(HTTP_PORT, onHttpStart());
 app.use((req, res) => {
     res.status(404).end('404 PAGE NOT FOUND');
 });
